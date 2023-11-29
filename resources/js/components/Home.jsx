54d8a87fd68492d2e8c5785c0c7fb4b80150cbef
ALexus <vorgen.morgen@yandex.ru>
2023-11-29 20:34:24 +0200

    Initial commit

---
+++ resources/js/components/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div className="continer">
+            <div className="my-5">
+                <div className="w-max mx-auto">
+                    <h1>Эта хуйня запустилась</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
