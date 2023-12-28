7e7a2c16dc6b57780e152a7d1dd5ad0a563d3056
legen <legend.bahh@gmail.com>
2023-12-28 12:47:59 +0200

    adding tables

--- database/migrations/2023_12_28_093051_create_topic_table.php
+++ database/migrations/2023_12_28_093051_create_topic_table.php
@@ -11,16 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('topic', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('login');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('image');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('name');
         });
     }
 
@@ -29,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('topic');
     }
 };
---
+++ database/migrations/2023_12_28_094455_create_article_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('description');
+            $table->string('image');
+            $table->string('tag');
+            $table->boolean('viewed');
+
+            $table->bigInteger('topic_id');
+            $table->bigInteger('user_id');
+
+            $table->foreign('topic_id')->references('id')->on('topic');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('article');
+    }
+};
