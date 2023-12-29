8a2ecdd7ea58278deedd201f282ffdb67f9a6128
ALexus <vorgen.morgen@yandex.ru>
2023-12-29 13:23:01 +0200

    Secured routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['jwt.auth','api-header']], function () {
+
+    // all routes to protected resources are registered here
+    Route::get('user/list', 'UserController@index')
+        ->middleware(['role:admin']);
+    Route::get('user/{user}', 'UserController@show');
+    Route::post('user','UserController@store');
+    Route::put('user/{user}','UserController@update');
+    Route::delete('user/{user}', 'UserController@delete');
+});
+
+Route::group(['middleware' => 'api-header'], function () {
+
+    // The registration and login requests doesn't come with tokens
+    // as users at that point have not been authenticated yet
+    // Therefore the jwtMiddleware will be exclusive of them
+
+    Route::post('user/login', 'UserController@login');
+    Route::post('user/register', 'UserController@register');
+});
+
+
