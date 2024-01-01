3052109b36a649987ec359adda82418e270d5784
ALexus <vorgen.morgen@yandex.ru>
2024-01-01 15:13:48 +0200

    ImageController.php

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -9,17 +9,9 @@ use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class ImageController extends Controller
 {
-    public function store(Request $request) {
-        //$this->validate($request, [
-        //    'image' => 'required|image|mimes:jpg,png,jpeg|max:2048'
-        //]); TODO PASTE IT TO THE ARTICLE CONTROLLER
+    public static function upload($request) {
+        $image_path = $request->store('image', 'public');
 
-        $image_path = $request->file('image')->store('image', 'public');
-
-        $data = Image::create([
-            'image' => $image_path,
-        ]);
-
-        return response()->json($data, 201);
+        return $image_path;
     }
 }
