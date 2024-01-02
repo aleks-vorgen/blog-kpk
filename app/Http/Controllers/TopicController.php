80854b4199f75dd60b595b11a4a823abea4ab61c
ALexus <vorgen.morgen@yandex.ru>
2024-01-02 21:24:32 +0200

    fix

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -55,7 +55,7 @@ class TopicController extends Controller
      */
     public function update(Request $request, $id) {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|min:5',
+            'name' => 'required|min:3',
         ]);
 
         if ($validator->fails()) {
