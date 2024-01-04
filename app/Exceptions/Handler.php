1d2d876ab38bf7c58fd9f238d991ed8c80b8a20c
legen <legend.bahh@gmail.com>
2024-01-04 21:37:45 +0200

    fixes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -19,7 +19,7 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * dasdasda
+     * Register the exception handling callbacks for the application.
      */
     public function register(): void
     {
